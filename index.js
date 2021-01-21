2f3f6e7210bf429a845c6198c632e9a54c4ea348
beans14 <75383685+beans14@users.noreply.github.com>
2021-01-21 15:24:02 -0500

    Update index.js

--- index.js
+++ index.js
@@ -20,8 +20,11 @@ client.on('message', message => {
     const args = message.content.slice(prefix.length).split(/ +/)
     const command = args.shift().toLowerCase();
     
-    if (message.content.startsWith(prefix + 'img')){
-        let results = 'cursed images';
+       if (message.content.startsWith(prefix + 'search')){
+        if (!args){
+          message.channel.send(`Please include what image you are searching for`)
+        }
+        let results = ${args};
         image(message, results);
     }
 
