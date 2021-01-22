9d8392c0e822ca1fe5ebbc627bdb8fd7502e0867
beans14 <75383685+beans14@users.noreply.github.com>
2021-01-22 10:51:25 -0500

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,48 @@ client.on('ready', () => {
   console.log('I am ready!');
 });
 
- 
+client.on('message', (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+  
+  if (message.content.startsWith(prefix + "gif"))
+  {
+    
+    let splitWord = message.toString().split(" ");
+    let gifWord   = "";
+
+    
+    for( var i = 1; i < splitWord.length; i++)
+    {
+      if(i > 1)
+      {
+        gifWord = gifWord + "+";
+      }
+
+      gifWord = gifWord + splitWord[i];
+    }
+
+    request("http://api.giphy.com/v1/gifs/search?q=" + gifWord + "&api_key=" + config.giphyKey + "&limit=100", function (error, response, body)
+    {
+      if (!error && response.statusCode == 200)
+      {
+        // Convert body to JSON object
+        let jsonUrl = JSON.parse(body);
+
+        // Get random number to choose GIF
+        let totGif = jsonUrl.data.length;
+
+        if(totGif > 100)
+        {
+          totGif = 100;
+        }
+
+        let ranNum = Math.floor(Math.random() * totGif);
+
+        message.channel.sendMessage(jsonUrl.data[ranNum].url);
+      }
+    });
+  }
+   
    
  
 
